59575f74918c328b449083afb9c09fb6ee35171a
krzysztof.tokarski <krzysztof.tokarski96@gmail.com>
2023-11-21 21:51:32 +0100

    add ex02

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,12 @@
+void ft_print_reverse_alphabet(void) {
+    char i = 'z';
+    while (i >= 'a') {
+        write(1, &i, 1);
+        i--;
+    }
+}
+
+int main()
+{
+	ft_print_reverse_alphabet();
+}
